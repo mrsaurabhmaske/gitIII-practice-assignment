f0edf0e2dfb5255999862d6d258f8a38210bbef5
SauRabh MaSke <saurabh.r.maske@gmail.com>
2023-02-17 16:48:19 +0600

    Member1 correcter the code

--- PrimeProblem.js
+++ PrimeProblem.js
@@ -1,13 +1,13 @@
 //Progrmam to check if a number is Prime or not.
 
 let num=23;
-
+counter=0;
 for(let i=1;i<=num/2;i++){
 	if(num%i==0){
-		count--;		//Error
+		counter++;		//Error Fixed
 	}
 }
-if(count==1){
+if(counter==1){
 	console.log("Given number:",num,"is Prime");
 }
 else{
